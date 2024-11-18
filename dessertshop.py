ab06023469897388b5c3c49c89f3869c8fcc1f14
Git-Hub-Coder-user <elsa.schultheiss@ucas-edu.net>
2024-11-18 11:48:58 -0700

    Finished dessert shop part 4, hypothetically

--- dessertshop.py
+++ dessertshop.py
@@ -36,10 +36,126 @@ class Order:
             tax += item.calculate_tax()
         return tax
 
-class DessertStop:
-    def __init__():
+class DessertShop:
+    def __init__(self):
         pass
+
+    def user_prompt_candy(self):
+        while True: 
+            try: 
+                stored = Candy("Temp")
+                stored.name = input("Enter the type of candy: ")
+                stored.candy_weight = float(input("Enter the weight of candy: "))
+                stored.price_per_pound = float(input("Enter the price per pound: "))
+                return stored
+            except:
+                print("That input was invalid. ")
+
+    def user_prompt_cookie(self):
+        while True:
+            try: 
+                stored = Cookie("Temp")
+                stored.name = input("Enter the type of cookie: ")
+                stored.quantity = int(input("Enter the quantity purchased: "))
+                stored.price_per_dozen = float(input("Enter the price per dozen: "))
+                return stored
+            except:
+                print("That input was invalid. ")
+
+    def user_prompt_icecream(self):
+        while True:
+            try: 
+                stored = IceCream("Temp")
+                stored.name = input("Enter the type of ice cream: ")
+                stored.scoop_count = int(input("Enter the number of scoops: "))
+                stored.price_per_scoop = float(input("Enter the price per scoop: "))
+                return stored
+            except:
+                print("That input was invalid. ")
+
+    def user_prompt_sundae(self):
+        while True:
+            try: 
+                stored = Sundae("Temp")
+                stored.name = input("Enter the type of ice cream: ")
+                stored.scoop_count = int(input("Enter the number of scoops: "))
+                stored.price_per_scoop = float(input("Enter the price per scoop: "))
+                stored.topping_name = input("Enter the topping: ")
+                stored.topping_name = float(input("Enter the price for the topping. "))
+                return stored
+            except:
+                print("That input was invalid. ")
+
+
+
+'''
+Code to implement the main loop of terminal-based user interface for
+Dessert Shop Part 4. Students should be able to paste this code into their own
+main() method as-is and use it without change.
+Author: George Rudolph
+Date: 2 Jun 2023
+'''
+def main():
+    shop = DessertShop()
+    order = Order()
+    listed = []
+    sub_total = 0
+    tax = 0
+    # boolean done = false
+    done: bool = False
+    # build the prompt string once
+    prompt = '\n'.join([ '\n',
+    '1: Candy',
+    '2: Cookie',
+    '3: Ice Cream',
+    '4: Sundae',
+    '\nWhat would you like to add to the order? (1-4, Enter for done): '
+    ])
+    while not done:
+
+        choice = input(prompt)
+        match choice:
+            case '':
+                done = True
+            case '1':
+                item = shop.user_prompt_candy()
+                order.add(item)
+                print(f'{item.name} has been added to your order.')
+            case '2':
+                item = shop.user_prompt_cookie()
+                order.add(item)
+                print(f'{item.name} has been added to your order.')
+            case '3':
+                item = shop.user_prompt_icecream()
+                order.add(item)
+                print(f'{item.name} has been added to your order.')
+            case '4':
+                item = shop.user_prompt_sundae()
+                order.add(item)
+                print(f'{item.name} has been added to your order.')
+            case _:
+                print('Invalid response: Please enter a choice from the menu (1-4) or Enter')
+        print()
+
+    for item in order.order:
+        listed.append([item.name, round(item.calculate_cost(), 2), round(item.calculate_tax(), 2)])
+        sub_total += item.calculate_cost()
+        tax += item.calculate_tax()
+
+    total_cost = sub_total + tax
+    total_number_of_items_in_the_order = len(order)
+
+    listed.append(["Order Subtotals: ", "$" + str(round(sub_total, 2)), "$" + str(round(tax, 2))])
+    listed.append(["Order Total:", "", "$" + str(round(total_cost, 2))])
+    listed.append(["Total items in the order: ","", total_number_of_items_in_the_order])
+
+
     
+    make_receipt(listed, "receipt")
+
+
+#Old main
+"""
 def main():
     listed = []
     sub_total = 0
@@ -68,5 +184,6 @@ def main():
     listed.append(["Total items in the order: ","", total_number_of_items_in_the_order])
 
     make_receipt(listed, "receipt")
+"""
 
 main()
